aad49e53c45f5af7bf9128a96f4188271ef446b4
Alexander Zhebrak <fata2ex@gmail.com>
2016-01-25 20:17:04 +0300

    repeat mode, refactoring, testing

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 # coding: utf-8
 
+import argparse
 import configparser
 import json
 import logging
 import random
 import re
+
+from functools import partial
 
 from telegram import Updater
 
@@ -16,71 +19,128 @@ logging.basicConfig(
 )
 logger = logging.getLogger('reduplicator')
 
-config = configparser.ConfigParser()
-config.read(['reduplicator.conf'])
-
 
 class ReduplicatorException(Exception):
     pass
 
 
-def reduplicate(bot, update):
-    try:
-        if update.message.chat.type == 'group' and not re.search(bot.username, update.message.text, flags=re.I):
-            return
+def in_groups_only_answer_if_called_by_name(func):
+        def inner(self, bot, update):
+            if not update.message.chat.type == 'group' or re.search(bot.username, update.message.text, flags=re.I):
+                return func(self, bot, update)
 
-        regex = re.compile('[а-яА-Я]*')
-        message = regex.sub(reduplicate_word, update.message.text)
-        if message == update.message.text:
-            raise ReduplicatorException
+        return inner
 
-        message = re.sub('^@{} '.format(bot.username), '', message, flags=re.I)
+
+class Reduplicator(object):
+    CONFIG_PATH = 'reduplicator.conf'
+
+    def __init__(self, token_key):
+        self.config = configparser.ConfigParser()
+        self.config.read([self.CONFIG_PATH])
+
+        self.updater = Updater(token=self.config.get('main', token_key))
+        self.dispatcher = self.updater.dispatcher
+        self.bot = self.updater.bot
+
+        self.mode_map = {
+            'repeat': {}
+        }
+
+    def start(self):
+        self.dispatcher.addTelegramCommandHandler("help", self.get_help)
+        self.dispatcher.addTelegramCommandHandler("start", self.get_help)
+
+        self.dispatcher.addTelegramCommandHandler("repeat", self.repeat_mode)
+        self.dispatcher.addTelegramCommandHandler("norepeat", self.norepeat_mode)
+
+        self.dispatcher.addTelegramRegexHandler("^[^/].*", self.reduplicate)
+        self.dispatcher.addErrorHandler(self.error)
+
+        self.updater.start_polling()
+        self.updater.idle()
+
+    @in_groups_only_answer_if_called_by_name
+    def get_help(self, bot, update):
+        message = 'Лексическая редупликация — фономорфологическое явление, ' \
+                  'состоящее в удвоении начального слога, основы (полностью или частично) или слова.\n\n' \
+                  '/help — для вывода помощи\n' \
+                  '/repeat — для включения режима повторения\n' \
+                  '/norepeat — для выключения режима повторения'
         bot.sendMessage(update.message.chat_id, text=message)
 
-    except ReduplicatorException:
-        choices = json.loads(config.get('main', 'answers'))
-        bot.sendMessage(update.message.chat_id, text=random.choice(choices))
+    @in_groups_only_answer_if_called_by_name
+    def repeat_mode(self, bot, update):
+        self.mode_map['repeat'][update.message.chat_id] = True
+        bot.sendMessage(update.message.chat_id, text='Режим повторения включен!')
 
+    @in_groups_only_answer_if_called_by_name
+    def norepeat_mode(self, bot, update):
+        self.mode_map['repeat'][update.message.chat_id] = False
+        bot.sendMessage(update.message.chat_id, text='Режим повторения выключен!')
 
-def reduplicate_word(match_object):
-    vowels = 'аеёиоуыэюя'
-    vowel_map = {
-        'а': 'я',
-        'о': 'ё',
-        'у': 'ю',
-        'э': 'е',
-        'ы': 'и'
-    }
+    @in_groups_only_answer_if_called_by_name
+    def reduplicate(self, bot, update):
+        try:
+            regex = re.compile('[а-яА-Я]*')
+            replacer = partial(self.reduplicate_word, repeat=self.mode_map['repeat'].get(update.message.chat_id))
+            message = regex.sub(replacer, update.message.text)
 
-    word = match_object.group(0)
+            if message == update.message.text:
+                raise ReduplicatorException
 
-    def replacer(inner_match_object):
-        if not inner_match_object.end(0) == len(word):
-            letter = inner_match_object.group(0)[-1]
-            replaced = 'ху{}'.format(vowel_map.get(letter, letter))
+            message = re.sub('^@{} '.format(bot.username), '', message, flags=re.I)
+            bot.sendMessage(update.message.chat_id, text=message)
 
-            if inner_match_object.group(0)[0].isupper():
-                replaced = replaced.capitalize()
+        except ReduplicatorException:
+            choices = json.loads(self.config.get('main', 'answers'))
+            bot.sendMessage(update.message.chat_id, text=random.choice(choices))
 
-            return replaced
+    @staticmethod
+    def reduplicate_word(match_object, repeat=False):
+        vowels = 'аеёиоуыэюя'
+        vowel_map = {
+            'а': 'я',
+            'о': 'ё',
+            'у': 'ю',
+            'э': 'е',
+            'ы': 'и'
+        }
 
-    regex = re.compile('[^{}]*([{}])'.format(vowels, vowels), flags=re.I)
-    return regex.sub(replacer, word, 1) or word
+        word = match_object.group(0)
 
+        def replacer(inner_match_object):
+            if not inner_match_object.end(0) == len(word):
+                letter = inner_match_object.group(0)[-1].lower()
+                replaced = 'ху{}'.format(vowel_map.get(letter, letter))
 
-def error(bot, update, error):
-    logger.warn('\n'.join([update, error]))
+                if inner_match_object.group(0)[0].isupper():
+                    replaced = replaced.capitalize()
+
+                if repeat:
+                    replaced = '{}-{}'.format(word, replaced)
+
+                return replaced
+
+        regex = re.compile('[^{}]*([{}])'.format(vowels, vowels), flags=re.I)
+        return regex.sub(replacer, word, 1) or word
+
+    def error(self, bot, update, error):
+        logger.warn('\n'.join([update, error]))
 
 
 def main():
-    updater = Updater(token=config.get('main', 'token'))
-    dp = updater.dispatcher
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action='store_true', help="for using test bot")
+    args = parser.parse_args()
 
-    dp.addTelegramRegexHandler(".*", reduplicate)
-    dp.addErrorHandler(error)
+    token_key = 'token'
+    if args.test:
+        token_key = 'token_test'
 
-    updater.start_polling()
-    updater.idle()
+    bot = Reduplicator(token_key)
+    bot.start()
+
 
 if __name__ == '__main__':
     main()
